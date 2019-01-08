118bdbb857806c44266a4ca8853a7c890923ab27
scirit <sonercirit@gmail.com>
2019-01-09 00:14:21 +0300

    adds helper method for getting values from config

--- lib/news_ingester.ex
+++ lib/news_ingester.ex
@@ -1,2 +1,10 @@
 defmodule NewsIngester do
+  @moduledoc false
+
+  @doc """
+  Gets value from config using key
+  """
+  def get_config(key) do
+    Application.get_env(:news_ingester, key)
+  end
 end
