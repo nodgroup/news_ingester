039f440d5ff68fd04bee6df49c331bde944bb408
scirit <sonercirit@gmail.com>
2019-01-09 00:35:13 +0300

    adds helper method for generating http auth header for requests

--- lib/news_ingester.ex
+++ lib/news_ingester.ex
@@ -7,4 +7,13 @@ defmodule NewsIngester do
   def get_config(key) do
     Application.get_env(:news_ingester, key)
   end
+
+  @doc """
+  Generates http basic authentication header from config
+  """
+  def generate_auth_header do
+    username = get_config(:api_username)
+    password = get_config(:api_password)
+    {'Authorization', 'Basic ' ++ :base64.encode_to_string('#{username}:#{password}}')}
+  end
 end
