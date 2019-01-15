faac9e4f51a983f3a59fa339c8353be45b96e1f7
scirit <sonercirit@gmail.com>
2019-01-15 05:04:07 +0300

    fixes bug where exUnit started whole application

--- mix.exs
+++ mix.exs
@@ -7,6 +7,7 @@ defmodule NewsIngester.MixProject do
       version: "0.1.0",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
+      aliases: aliases(),
       deps: deps()
     ]
   end
@@ -33,6 +34,12 @@ defmodule NewsIngester.MixProject do
       {:neuron, "~> 1.1.0"},
       # for logging to file
       {:logger_file_backend, "~> 0.0.10"}
+    ]
+  end
+
+  defp aliases do
+    [
+      test: "test --no-start"
     ]
   end
 end
--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,1 +1,7 @@
+Application.load(:news_ingester)
+
+for app <- Application.spec(:news_ingester, :applications) do
+  Application.ensure_all_started(app)
+end
+
 ExUnit.start()
