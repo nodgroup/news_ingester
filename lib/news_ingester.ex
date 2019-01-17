04bb454fdc230f3192d59758a41035f613f0c491
scirit <sonercirit@gmail.com>
2019-01-18 01:18:43 +0300

    formats create table function

--- lib/news_ingester.ex
+++ lib/news_ingester.ex
@@ -12,10 +12,13 @@ defmodule NewsIngester do
   Creates DynamoDB table and raises error on unhandled exception
   """
   def create_table(table_name, primary_key, pk_type) do
-    tables = ExAws.Dynamo.list_tables() |> ExAws.request!()
+    tables =
+      ExAws.Dynamo.list_tables()
+      |> ExAws.request!()
 
     if Enum.member?(tables["TableNames"], table_name) == false do
-      ExAws.Dynamo.create_table(table_name, primary_key, %{"#{primary_key}": pk_type}, 1, 1)
+      table_name
+      |> ExAws.Dynamo.create_table(primary_key, %{"#{primary_key}": pk_type}, 1, 1)
       |> ExAws.request!()
     end
   end
