5c710f529e5785f2a1315205c4a31582ad214260
scirit <sonercirit@gmail.com>
2019-01-18 22:43:14 +0300

    adds text tests

--- test/news_ingester/a_a_crawler_test.exs
+++ test/news_ingester/a_a_crawler_test.exs
@@ -25,4 +25,19 @@ defmodule NewsIngester.AACrawlerTest do
     content_type = headers["Content-Type"]
     assert content_type == "image/jpeg"
   end
+
+  test "should get text", %{server: server} do
+    result = NewsIngester.AACrawler.search(server, true)
+
+    id =
+      result
+      |> Enum.find_value(fn {_k, v} ->
+        v |> Enum.find(fn v -> String.contains?(v, "text") end)
+      end)
+
+    result = NewsIngester.AACrawler.get_document(id, "text")
+    headers = Enum.into(result.headers, %{})
+    content_type = headers["Content-Type"]
+    assert content_type == "application/xml"
+  end
 end
